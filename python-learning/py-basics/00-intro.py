f888d89213aad626d0f21f6fa17f1262c48df390
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-31 10:42:04 +0530

    python learning

---
+++ python-learning/py-basics/00-intro.py
@@ -0,0 +1,32 @@
+import math
+
+# comments 
+
+print('Start')
+
+eid = 101
+firstName = 'Sonu'
+salary = 10.5
+
+# print(eid, firstName, salary)
+print(firstName)
+print(firstName[0])
+print(firstName[1:3])
+print(len(firstName))
+
+print('End')
+
+# if (10 > 20):
+#     print('yes')
+
+# val = 10
+
+# print(val)
+
+# val = 'abc'
+
+# print(val)
+
+# val = "def"
+
+# print(val)
---
+++ python-learning/py-basics/01-basics.py
@@ -0,0 +1,62 @@
+
+#  taking cla
+
+# import sys
+
+# args = sys.argv[1:]
+
+# print(args)
+
+# user input 
+
+# user_input = input("Please enter a value: ")
+
+# print(user_input)
+
+# array types -- list, tuple, dictionary 
+
+# # list 
+# myList = [10, 20, 30.5, 'abc', "false"]
+
+# # tuple 
+# myTuple = (10, 20, 30.5, 'abc', "false")
+
+# # dictionary 
+# myDict = {"firstName": "Sonu", "salary": 10}
+
+# # set
+# mySet = {22, 31, 9, 25, 22, 31}
+
+# print(mySet)
+
+# for elem in myList:
+#     print(elem)
+
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(4, 8, 1):
+#     print(i)
+
+
+# operators in python 
+
+a = 10
+b = 3
+
+print(a * b)
+print(a ** b)
+print(a / b)
+print(a // b)
+
+
+
+
+
+
+
+
+
+
+
