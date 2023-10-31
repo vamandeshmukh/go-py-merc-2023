b056e20b1e1af9c30da0d868463601bc10a05b73
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-31 09:35:18 +0530

    go learning

--- go-learning/go-basics/02-error-handling.go
+++ go-learning/go-basics/02-error-handling.go
@@ -1,26 +1,47 @@
-// 02-error-handling.go
 package main
 
 import (
 	"fmt"
-	"errors"
 )
 
 func main() {
 	fmt.Println("Start")
 
-	output, err := divNums(10, 0)
-	fmt.Println(output)
-	if err != nil {
-		fmt.Println(err)
-}
+	friends := []string {"Sonu", "Monu", "Tonu"}
+
+	ind := 1
+	if ind < len(friends) {
+		fmt.Println(friends[ind])
+	}
+
 	fmt.Println("End")
 }
 
-func divNums (a, b int) (int, error) {
-	if ( b == 0) {
-		return 0, errors.New("do not divide by 0")
-	}
-	return a / b, nil
-}
+
+
+// // 02-error-handling.go
+// package main
+
+// import (
+// 	"fmt"
+// 	"errors"
+// )
+
+// func main() {
+// 	fmt.Println("Start")
+
+// 	output, err := divNums(10, 0)
+// 	fmt.Println(output)
+// 	if err != nil {
+// 		fmt.Println(err)
+// }
+// 	fmt.Println("End")
+// }
+
+// func divNums (a, b int) (int, error) {
+// 	if ( b == 0) {
+// 		return 0, errors.New("do not divide by 0")
+// 	}
+// 	return a / b, nil
+// }
 
