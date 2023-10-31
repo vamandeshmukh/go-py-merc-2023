ce2f4f5984c8927c35281124e15809481cd58e28
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-31 16:17:02 +0530

    python learning

--- python-learning/py-adv/02-wsgi-demo.py
+++ python-learning/py-adv/02-wsgi-demo.py
@@ -10,8 +10,8 @@ def hello_wsgi_app(environ, start_response):
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
-    httpd = make_server('', 8000, hello_wsgi_app)
-    print("Serving on port 8000...")
+    httpd = make_server('', 9999, hello_wsgi_app)
+    print("Serving on port 9999...")
     httpd.serve_forever()
 
 print('end')
